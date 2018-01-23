be8aaa7f204304739103245edd0721f9068f12b0
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-23 17:57:42 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,5 +17,7 @@ def speak_to_grandma(string)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase && string != "I LOVE YOU GRANDMA!"
     return "NO, NOT SINCE 1938!"
+  else string != string.upcase 
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
